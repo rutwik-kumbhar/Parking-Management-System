6f91e26c832ec80871be560958b4f067c3b5c04a
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2024-03-08 22:28:23 +0530

    initial Project Setup

---
+++ Parking-Management-Backend/src/main/java/com/parking/ParkingManagementBackend/ParkingManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.parking.ParkingManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementBackendApplication.class, args);
+	}
+
+}
---
+++ Parking-Management-Backend/src/test/java/com/parking/ParkingManagementBackend/ParkingManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.ParkingManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
